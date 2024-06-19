ffd065f159e4faca6591dce67746f5a63ff80c96
Sergio <sergiotejedarex@gmail.com>
2024-06-19 12:20:53 -0700

    DTO

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTaskDto {}
+export class CreateTaskDto {
+    
+userId: "number"
+title: "string"
+description: "string"
+dueDate: "date"
+priority: "string"
+status: "string"
+createdAt: "date"
+updatedAt: "date"
+categoryId: "number"
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,7 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    username: "string";
+    email: "string";
+    passwordHash: "string";
+    createdAt: "Date";
+    updatedAt: "Date";
+}
