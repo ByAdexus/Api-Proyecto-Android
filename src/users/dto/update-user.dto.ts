3b355ac12d7760ea60c3c936a8d386db6980cc6e
ByAdexus <sergiotejedarex@gmail.com>
2024-06-23 17:14:18 -0700

    pequeño cambio

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,9 +2,9 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    readonly username?: string;
-  readonly email?: string;
-  readonly passwordHash?: string;
-  readonly createdAt?: Date;
-  readonly updatedAt?: Date;
+  username?: string;
+  email?: string;
+  passwordHash?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
